9513e8bbdefd4d20d7aa7ca8572a4d435e3204d9
Zolender <ndeingare@gmail.com>
2025-03-01 15:32:38 +0200

    creating and encryption and decryption function for the caesar cipher.Need to be improved later

---
+++ Caesar_cipher.py
@@ -0,0 +1,38 @@
+def encryption_algo(text: str, shift: int) -> str:
+    
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÁÉÍÓÚáéíóú"
+
+    encrypted = ""
+    
+    for i in text.lower():
+        if i == " ":
+            encrypted += " "
+        else:
+            index = chars.find(i)
+            new_index = (index + shift) % len(chars)
+            encrypted += chars[new_index] 
+
+    return encrypted
+
+def decryption_algo(text: str, shift: int) -> str: 
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÁÉÍÓÚáéíóú"
+
+    decrypted = ""
+    
+    for i in text.lower():
+        if i == " ":
+            decrypted += " "
+        else:
+            index = chars.find(i)
+            new_index = (index + shift) % len(chars)
+            decrypted += chars[new_index] 
+
+    return decrypted
+
+
+
+message = "Look how dumb you are"
+
+print("The encrypted form: " , encryption_algo(message, 1))
+
+print("The decrypted form: ", decryption_algo(encryption_algo(message, 1), -1))
